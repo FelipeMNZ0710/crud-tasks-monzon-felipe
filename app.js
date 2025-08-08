434f72372f303807648f81dd57ac54e6de5d5e96
Felipe <felipemonzon0710@gmail.com>
2025-08-08 11:39:55 -0300

    configuración del servidor Express inicial y variables de entorno

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './src/routes/user.routes.js';
+import taskRoutes from './src/routes/task.routes.js';
+import sequelize from './src/config/database.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+
+const startServer = async () => {
+  try {
+
+    await sequelize.sync(); 
+    console.log('Conexión a la base de datos establecida correctamente.');
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
+    });
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
+  }
+};
+
+startServer();
